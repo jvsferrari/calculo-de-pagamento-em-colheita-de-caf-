69597221802ed258b3b451e7d315cbbd030958a1
jvsferrari <jvsferrari@gmail.com>
2025-12-26 21:36:19 -0300

    working keypad numbers and erase

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const DATABANK_NAME = "allData" 
 const nameField = document.getElementById("name");
-const pricePerField = document.getElementById("pricePer");
 const latoesField = document.getElementById("latoes");
 const litersField = document.getElementById("liters");
 
-function type(key, field){
-    field.value = field.value + key 
+function typeNum(key, fieldId){
+    const pricePerField = document.getElementById(fieldId)
+    pricePerField.value = pricePerField.value + key
 }
 
-function erase(field){
-    field.value = field.value.slice(0, -1)
+function erase(fieldId){
+    const pricePerField = document.getElementById(fieldId);
+    pricePerField.value = pricePerField.value.slice(0, -1)
 }
 
 function readDatabase(){
@@ -40,7 +41,7 @@ function addName(){
     saveData(list)
 }
 
-function getNumber(event, field, number){
+function getNumber(event, field, val){
 
     if (isNaN(field.value)){
         event.preventDefault();
@@ -48,31 +49,28 @@ function getNumber(event, field, number){
         return
     }
 
-    const number = parseInt(field.value);
+    val = parseInt(field.value);
 
     const list = readDatabase()
 
     const lastPerson = list[list.length - 1];
 
-    lastPerson[number] = parseInt(number)
+    lastPerson[val] = parseInt(val)
 
     saveData(list)
 }
 
 function calculateCost(priceTyped, latoesTyped, litersTyped){
     const pricePerTyped = parseInt(pricePerField.value);
-    const latoesTyped = parseInt(latoesField.value);
-    const litersTyped = parseInt(litersField.value);
+    latoesTyped = parseInt(latoesField.value);
+    litersTyped = parseInt(litersField.value);
     costCalculated = priceTyped/60 * (latoesTyped * 60 + litersTyped)
 
     list.push({
         costColumn: costCalculated
     })
 
-data.forEach(function(name)
-    
 }
-
 const finalTable = [
     {namesColumn:"", costColumn:""}
 ]
